85921a81eb31e87ef8d078922a273088ef986f2b
dms-mshilenko <shilenkomp@mail.ru>
2020-01-28 15:42:33 +0300

    add LessTestController, variables and mix-in for less

---
+++ resources/views/lessTest.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 gallery">
+            <div class="gallery-head">Галерея</div>
+            <div class="gallery-block">
+                <div class="g_item">1</div>
+                <div class="g_item">2</div>
+                <div class="g_item">3</div>
+                <div class="g_item">4</div>
+                <div class="g_item">5</div>
+                <div class="g_item">6</div>
+                <div class="g_item">7</div>
+                <div class="g_item">8</div>
+                <div class="g_item">9</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
